c1e6519443658243f6d4d3cc381b2d7d0eb5809c
spj0698 <spj0698@gmail.com>
2020-12-29 01:08:57 -0800

    progress

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export default [
+    {
+        id: 1,
+        name: 'Sagar Joshi',
+        age: 22,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+    }
+]
